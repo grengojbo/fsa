09d506d1d674e96155506ff72b135da54508cc3c
Oleg Dolya <oleg.dolya@gmail.com>
2010-02-20 22:23:20 +0200

    modify urls

--- fsa/urls/default.py
+++ fsa/urls/default.py
@@ -5,23 +5,36 @@ default.py
 Created by jbo on 2009-08-24.
 Copyright (c) 2009 __MyCompanyName__. All rights reserved.
 """
-
+from os.path import join, dirname
 from django.conf import settings
 from django.conf.urls.defaults import *
 
 import logging
 log = logging.getLogger('fsa.urls.default')
-log.debug("Adding local serving of admin files at: %s", settings.ADMIN_MEDIA_ROOT)
-urlpatterns = patterns('',
-    (r'^admin-media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.ADMIN_MEDIA_ROOT, 'show_indexes': True}),
-)
 
 #The following is used to serve up local media files like images
 if getattr(settings, 'LOCAL_DEV', False):
     log.debug("Adding local serving of static files at: %s", settings.MEDIA_ROOT)
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)', 'django.views.static.serve', {'document_root':settings.MEDIA_ROOT,'show_indexes': True}),
-        (r'^media/(?P<path>.*)', 'django.views.static.serve', {'document_root':settings.MEDIA_ROOT,'show_indexes': True}), 
-        
+    try:
+        import grappelli
+        urlpatterns = patterns('',
+            ('^admin-media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': join(dirname(grappelli.__path__[0]), 'grappelli/media')}),
+            (r'^static/(?P<path>.*)', 'django.views.static.serve', {'document_root':settings.MEDIA_ROOT,'show_indexes': True}),
+            (r'^media/(?P<path>.*)', 'django.views.static.serve', {'document_root':settings.MEDIA_ROOT,'show_indexes': True}), 
+        )
+        log.debug("Adding local serving of admin files at: %s",  join(dirname(grappelli.__path__[0]), 'grappelli/media'))
+    except:
+        from django.contrib import admin
+        urlpatterns = patterns('',
+            ('^admin-media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': join(dirname(admin.__file__), 'media')}),
+            (r'^static/(?P<path>.*)', 'django.views.static.serve', {'document_root':settings.MEDIA_ROOT,'show_indexes': True}),
+            (r'^media/(?P<path>.*)', 'django.views.static.serve', {'document_root':settings.MEDIA_ROOT,'show_indexes': True}), 
+        )
+        log.debug("Adding local serving of admin files at: %s", join(dirname(admin.__file__), 'media'))
+    #log.debug("Adding local serving of admin files at: %s", settings.ADMIN_MEDIA_ROOT)
+    #(r'^admin-media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.ADMIN_MEDIA_ROOT, 'show_indexes': True}),
+else:
+    log.debug("Adding local serving of admin files at: %s", settings.ADMIN_MEDIA_ROOT)
+    urlpatterns = patterns('',
+        (r'^admin-media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.ADMIN_MEDIA_ROOT, 'show_indexes': True}),
     )
-
