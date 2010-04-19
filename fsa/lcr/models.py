634c9a21238d023b53117dfe97fb9cf593cb6db9
Oleg Dolya <oleg.dolya@gmail.com>
2010-04-19 19:18:01 +0300

    modified:   fsa/lcr/models.py modified:   scripts/fsa-build new file:   scripts/npa-nxx-companytype-ocn.csv

--- fsa/lcr/models.py
+++ fsa/lcr/models.py
@@ -63,7 +63,7 @@ class Lcr(models.Model):
     suffix = models.CharField(_(u'Suffix'), max_length=100, blank=True, help_text=_(u'vaulue to add to end of passed in number'))
     lcr_profile = models.CharField(_(u'LCR Profile'), max_length=96, blank=True)
     date_start = models.DateTimeField(_(u'Date Start'), default=datetime.datetime.now())
-    #date_end = models.DateTimeField(_(u'Date End'))
+    date_end = models.DateTimeField(_(u'Date End'), default=datetime.datetime.max())
     quality = models.FloatField(_(u'Quality'), default=0, help_text=_(u'alternate field to order by'))
     reliability = models.FloatField(_(u'Reliability'), default=0, help_text=_(u'alternate field to order by'))
     enabled = models.BooleanField(_(u'Enable'), default=True)
