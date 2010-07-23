3d5d9673a6be51dd17f7afccd926421a2c32a827
Oleg Dolya <oleg.dolya@gmail.com>
2010-07-23 13:09:27 +0300

    modified:   fsa/lcr/api/handlers.py

--- fsa/lcr/api/handlers.py
+++ fsa/lcr/api/handlers.py
@@ -44,24 +44,28 @@ class LcrHandler(BaseHandler):
         if request.GET.get("limit"):
             limit = int(request.GET.get("limit"))
             limit += int(start)
-        try:
-            if phone is not None and si is not None:
-                log.info(phone)
-                #SELECT l.digits AS digits, cg.name AS gw, l.rate AS rate, cg.prefix AS gw_prefix, cg.suffix AS suffix, l.price AS price, l.price_currency AS currency, l.name AS name, l.lead_strip, l.trail_strip, l.prefix, l.suffix FROM lcr l LEFT JOIN carrier_gateway cg ON l.carrier_id_id=cg.id LEFT JOIN django_site s ON l.site_id=s.id WHERE cg.enabled = '1' AND l.enabled = '1' AND l.digits IN (380442, 380) AND CURRENT_TIMESTAMP BETWEEN l.date_start AND l.date_end AND CURTIME() BETWEEN l.time_start AND l.time_end AND (DAYOFWEEK(NOW()) = l.weeks OR l.weeks = 0) AND s.name="089.com.ua" ORDER BY  digits DESC, reliability DESC, quality DESC;
-                #, rand();
-                #select DAYOFWEEK(NOW()) IN (weeks);
-                # AND CURRENT_TIMESTAMP BETWEEN l.date_start AND l.date_end
-                query = "SELECT l.id AS id, l.digits AS digits, cg.name AS gw, l.rate AS rate, cg.prefix AS gw_prefix, cg.suffix AS suffix, l.price AS price, l.price_currency AS currency, l.name AS name FROM lcr l LEFT JOIN carrier_gateway cg ON l.carrier_id_id=cg.id LEFT JOIN django_site s ON l.site_id=s.id WHERE cg.enabled = '1' AND l.enabled = '1' AND l.digits IN (%s) AND CURTIME() BETWEEN l.time_start AND l.time_end AND (DAYOFWEEK(NOW()) = l.weeks OR l.weeks = 0) AND s.name='%s' ORDER BY  digits DESC, reliability DESC, quality DESC;" % (pars_phone(phone), si)
-                log.debug(query)
-                resp = base.raw(query)[0]
+        if phone is not None and si is not None:
+            log.info(phone)
+            #SELECT l.digits AS digits, cg.name AS gw, l.rate AS rate, cg.prefix AS gw_prefix, cg.suffix AS suffix, l.price AS price, l.price_currency AS currency, l.name AS name, l.lead_strip, l.trail_strip, l.prefix, l.suffix FROM lcr l LEFT JOIN carrier_gateway cg ON l.carrier_id_id=cg.id LEFT JOIN django_site s ON l.site_id=s.id WHERE cg.enabled = '1' AND l.enabled = '1' AND l.digits IN (380442, 380) AND CURRENT_TIMESTAMP BETWEEN l.date_start AND l.date_end AND CURTIME() BETWEEN l.time_start AND l.time_end AND (DAYOFWEEK(NOW()) = l.weeks OR l.weeks = 0) AND s.name="089.com.ua" ORDER BY  digits DESC, reliability DESC, quality DESC;
+            #, rand();
+            #select DAYOFWEEK(NOW()) IN (weeks);
+            # AND CURRENT_TIMESTAMP BETWEEN l.date_start AND l.date_end
+            query = "SELECT l.id AS id, l.digits AS digits, cg.name AS gw, l.rate AS rate, cg.prefix AS gw_prefix, cg.suffix AS suffix, l.price AS price, l.price_currency AS currency, l.name AS name FROM lcr l LEFT JOIN carrier_gateway cg ON l.carrier_id_id=cg.id LEFT JOIN django_site s ON l.site_id=s.id WHERE cg.enabled = '1' AND l.enabled = '1' AND l.digits IN (%s) AND CURTIME() BETWEEN l.time_start AND l.time_end AND (DAYOFWEEK(NOW()) = l.weeks OR l.weeks = 0) AND s.name='%s' ORDER BY  digits DESC, reliability DESC, quality DESC;" % (pars_phone(phone), si)
+            log.debug(query)
+            resp = base.raw(query)[0]
+            log.debug(resp)
                 #return {"rate": resp.rate, "suffix": resp.suffix, "digits": resp.digits, "gw": resp.gw, "price": resp.price, "currency": resp.currency, "name": resp.name }
-                return {"rate": resp.rate}
-            else:
-                resp = base.filter(tariff_plan__id=tariff, enabled=True, tariff_plan__site__name__exact=request.user)[start:limit]
-                count = base.filter(tariff_plan__id=tariff, enabled=True, tariff_plan__site__name__exact=request.user).count()
-                return {"count": count, "lcr": resp}
-        except:
-            return rc.NOT_HERE
+            return {"rate": resp.rate}
+        
+        
+        #try:
+            
+            #else:
+                #resp = base.filter(tariff_plan__id=tariff, enabled=True, tariff_plan__site__name__exact=request.user)[start:limit]
+                #count = base.filter(tariff_plan__id=tariff, enabled=True, tariff_plan__site__name__exact=request.user).count()
+                #return {"count": count, "lcr": resp}
+        #except:
+            #return rc.NOT_HERE
         #try:
             #if phone is not None:
                 #server = ServerProxy("http://%s:%s@%s:%s" % (config_value('SERVER', 'rcpuser'), config_value('SERVER', 'rcppasswd'), config_value('SERVER', 'rcphost'), config_value('SERVER', 'rcpport')))
